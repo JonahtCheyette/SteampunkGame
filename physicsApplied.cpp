1b5413fc239d713400d9a70cde3ec1f0d1cd2c83
JonahtCheyette <jonahcheyette@gmail.com>
2019-03-25 15:31:51 -0700

    added proper documentation to physicsApplied class

--- physicsApplied.cpp
+++ physicsApplied.cpp
@@ -1,15 +1,19 @@
 #include "stdafx.h"
 
 void physicsApplied::update(){
+    //add acceleration to velocity
     velX += accelX;
     velY += accelY;
+    //add velocity to position
     x += velX;
     y += velY;
+    //zero out velocity
     accelX = 0;
     accelY = 0;
 }
 
 void physicsApplied::applyForce(float fx, float fy){
+    //takes changes velocity based on forces
     accelX += (fx/mass);
     accelY += (fy/mass);
 }
--- physicsApplied.h
+++ physicsApplied.h
@@ -5,6 +5,7 @@ class physicsApplied {
 public:
     void update();
     void applyForce(float fx, float fy);
+    //position, velocity, acceleration, mass, and max speeds (not initialized in this class, but for every child class to decide what to initialize these to)
     float x, y;
     float velX, velY;
     float accelX, accelY;
